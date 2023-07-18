37553a8f168ebe6324c0969926c9a75c89431c11
AZ <47402666+realazaber@users.noreply.github.com>
2023-07-19 01:06:55 +0200

    Moved all logic to service

--- src/main/java/com/nphase/entity/ShoppingCart.java
+++ src/main/java/com/nphase/entity/ShoppingCart.java
@@ -7,52 +7,11 @@ import java.math.BigDecimal;
 public class ShoppingCart {
     private final List<Product> products;
 
-    // Configurable discount items
-    private final int discountItems = 3;
-
-    // Configurable discount rate
-    private final int discountRate = 10;
-
     public ShoppingCart(List<Product> products) {
         this.products = products;
     }
 
     public List<Product> getProducts() {
         return products;
-    }
-
-    public BigDecimal getTotal() {
-        BigDecimal total = BigDecimal.ZERO;
-        Map<String, Integer> productCounts = new HashMap<>();
-        Map<String, Integer> categoryCounts = new HashMap<>();
-
-        // Calculate the count of each product and category
-        for (var product : products) {
-            String productKey = product.getName();
-            productCounts.put(productKey, productCounts.getOrDefault(productKey, 0) + 1);
-
-            String categoryKey = product.getCategory();
-            categoryCounts.put(categoryKey, categoryCounts.getOrDefault(categoryKey, 0) + 1);
-
-            total = total.add(product.getPricePerUnit());
-        }
-
-        // Apply discountRate per product if same product is bought discountItems times or more
-        for (var entry : productCounts.entrySet()) {
-            if (entry.getValue() >= discountItems) {
-                BigDecimal productDiscount = entry.getKey().getPricePerUnit().multiply(BigDecimal.valueOf(discountRate)).divide(BigDecimal.valueOf(100));
-                total = total.subtract(productDiscount);
-            }
-        }
-
-        // Apply discountRate discount per product in category if bought discountItems times or more
-        for (var entry : categoryCounts.entrySet()) {
-            if (entry.getValue() >= discountItems) {
-                BigDecimal categoryDiscount = entry.getKey().getCategory().multiply(BigDecimal.valueOf(discountRate)).divide(BigDecimal.valueOf(100));
-                total = total.subtract(categoryDiscount);
-            }
-        }
-
-        return total;
     }
 }
--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,55 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShoppingCartService {
 
+    private static final int DISCOUNT_ITEMS = 3;
+    private static final int DISCOUNT_RATE = 10;
+
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
-        return shoppingCart.getProducts()
-                .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
-                .reduce(BigDecimal::add)
-                .orElse(BigDecimal.ZERO);
+        BigDecimal total = BigDecimal.ZERO;
+        Map<String, Integer> productCounts = new HashMap<>();
+        Map<String, Integer> categoryCounts = new HashMap<>();
+
+        for (Product product : shoppingCart.getProducts()) {
+            String productKey = product.getName();
+            productCounts.put(productKey, productCounts.getOrDefault(productKey, 0) + 1);
+
+            String categoryKey = product.getCategory();
+            categoryCounts.put(categoryKey, categoryCounts.getOrDefault(categoryKey, 0) + 1);
+
+            total = total.add(product.getPricePerUnit());
+        }
+
+        total = applyProductDiscounts(total, productCounts);
+        total = applyCategoryDiscounts(total, categoryCounts);
+
+        return total;
+    }
+
+    private BigDecimal applyProductDiscounts(BigDecimal total, Map<String, Integer> productCounts) {
+        for (Map.Entry<String, Integer> entry : productCounts.entrySet()) {
+            if (entry.getValue() >= DISCOUNT_ITEMS) {
+                BigDecimal productDiscount = total.multiply(BigDecimal.valueOf(DISCOUNT_RATE)).divide(BigDecimal.valueOf(100));
+                total = total.subtract(productDiscount);
+            }
+        }
+        return total;
+    }
+
+    private BigDecimal applyCategoryDiscounts(BigDecimal total, Map<String, Integer> categoryCounts) {
+        for (Map.Entry<String, Integer> entry : categoryCounts.entrySet()) {
+            if (entry.getValue() >= DISCOUNT_ITEMS) {
+                BigDecimal categoryDiscount = total.multiply(BigDecimal.valueOf(DISCOUNT_RATE)).divide(BigDecimal.valueOf(100));
+                total = total.subtract(categoryDiscount);
+            }
+        }
+        return total;
     }
 }
